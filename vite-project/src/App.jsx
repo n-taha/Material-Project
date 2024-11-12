477057db3baaf6238bbd02e5afd159e2ce1ad863
n-taha <mubtasimtaha@gmail.com>
2024-11-12 19:44:47 +0600

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+import Navbar from '../Components/Navbar';
+import Hero from '../Components/Hero';
+import Presentation from '../Components/Presentation';
+
+
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Presentation />
+
+    </>
+  )
+}
+
+export default App
